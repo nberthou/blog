bad6e430140b315f630ad188dc8c366422d90b33
nberthou <berthou.nicolas@laposte.net>
2026-01-16 13:39:01 +0100

    ADDED submit button and input errors

--- resources/js/components/AuthForm.vue
+++ resources/js/components/AuthForm.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 
 import { Form } from '@inertiajs/vue3';
-import { store } from "@/routes/login";
+import { store as loginStore } from "@/routes/login";
+import { store as registerStore } from "@/routes/register";
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Spinner} from "@/components/ui/spinner";
+import InputError from "@/components/InputError.vue";
 
 type Props = {
     type: "LOGIN" | "REGISTER"
@@ -12,8 +16,15 @@ type Props = {
 const props = defineProps<Props>();
 
 const formFields = {
-    LOGIN: [{name: 'email', label: 'Adresse mail', placeholder: 'email@exemple.com', type: 'text'}, {name: 'password', label: 'Mot de passe', placeholder: 'Mot de passe', type: 'text'}, {name: 'remember', label: 'Se souvenir de moi', placeholder: null, type: 'checkbox'}],
-    REGISTER: [{name: 'name', label: 'Nom', placeholder: 'Nom complet', type: 'text'}, {name: 'email', label: 'Adresse mail', placeholder: 'email@exemple.com', type: 'text'}, {name: 'password', label: 'Mot de passe', placeholder: 'Mot de passe', type: 'text'}, {name: 'password_confirmation', label: 'Confirmer le mot de passe', placeholder: 'Confirmer le mot de passe', type: 'text'}],
+    LOGIN: [{name: 'email', label: 'Adresse mail', placeholder: 'email@exemple.com', type: 'text'}, {name: 'password', label: 'Mot de passe', placeholder: 'Mot de passe', type: 'password'}, {name: 'remember', label: 'Se souvenir de moi', placeholder: null, type: 'checkbox'}],
+    REGISTER: [{name: 'name', label: 'Nom', placeholder: 'Nom complet', type: 'text'}, {name: 'email', label: 'Adresse mail', placeholder: 'email@exemple.com', type: 'text'}, {name: 'password', label: 'Mot de passe', placeholder: 'Mot de passe', type: 'password'}, {name: 'password_confirmation', label: 'Confirmer le mot de passe', placeholder: 'Confirmer le mot de passe', type: 'password'}],
+};
+
+const store = props.type === "LOGIN" ? loginStore : registerStore;
+
+const buttonLabels = {
+    LOGIN: "Se connecter",
+    REGISTER: "S'inscrire"
 };
 
 </script>
@@ -37,8 +48,14 @@ const formFields = {
                         :name="field.name"
                         :placeholder="field.placeholder"
                     />
+                    <InputError :message="errors[field.name]" />
                 </div>
         </div>
+
+        <Button type="submit" class="mt-2 w-full" tabindex="5" :disabled="processing">
+            <Spinner v-if="processing" />
+            {{ buttonLabels[type] }}
+        </Button>
     </Form>
 </template>
 
