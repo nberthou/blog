9b92ac6be24e0b9702fc674a151920a4f0ca5911
nberthou <berthou.nicolas@laposte.net>
2026-01-20 22:28:05 +0100

    FIXED remember me checkbox

--- resources/js/components/AuthForm.vue
+++ resources/js/components/AuthForm.vue
@@ -7,6 +7,7 @@ import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {Spinner} from "@/components/ui/spinner";
+import {Checkbox} from "@/components/ui/checkbox";
 import InputError from "@/components/InputError.vue";
 
 type Props = {
@@ -19,7 +20,7 @@ const emit = defineEmits<{
 }>();
 
 const formFields = {
-    LOGIN: [{name: 'email', label: 'Adresse mail', placeholder: 'email@exemple.com', type: 'text'}, {name: 'password', label: 'Mot de passe', placeholder: 'Mot de passe', type: 'password'}, {name: 'remember', label: 'Se souvenir de moi', placeholder: null, type: 'checkbox'}],
+    LOGIN: [{name: 'email', label: 'Adresse mail', placeholder: 'email@exemple.com', type: 'text'}, {name: 'password', label: 'Mot de passe', placeholder: 'Mot de passe', type: 'password'}],
     REGISTER: [{name: 'name', label: 'Nom', placeholder: 'Nom complet', type: 'text'}, {name: 'email', label: 'Adresse mail', placeholder: 'email@exemple.com', type: 'text'}, {name: 'password', label: 'Mot de passe', placeholder: 'Mot de passe', type: 'password'}, {name: 'password_confirmation', label: 'Confirmer le mot de passe', placeholder: 'Confirmer le mot de passe', type: 'password'}],
 };
 
@@ -53,20 +54,31 @@ function submit() {
 <template>
     <form @submit.prevent="submit" class="flex flex-col gap-6">
         <div class="grid gap-6">
-                <div class="grid gap-2" v-for="field in formFields[type]" :key="field.name">
-                    <Label :for="field.name">{{field.label}}</Label>
-                    <Input
-                        :id="field.name"
-                        v-model="(form as any)[field.name]"
-                        :type="field.type"
-                        :required="field.type !== 'checkbox'"
-                        autofocus
-                        :tabindex="1"
-                        :name="field.name"
-                        :placeholder="field.placeholder ?? undefined"
-                    />
-                    <InputError :message="form.errors[field.name]" />
-                </div>
+            <div class="grid gap-2" v-for="field in formFields[type]" :key="field.name">
+                <Label :for="field.name">{{field.label}}</Label>
+                <Input
+                    :id="field.name"
+                    v-model="(form as any)[field.name]"
+                    :type="field.type"
+                    required
+                    autofocus
+                    :tabindex="1"
+                    :name="field.name"
+                    :placeholder="field.placeholder ?? undefined"
+                />
+                <InputError :message="form.errors[field.name]" />
+            </div>
+
+            <div v-if="type === 'LOGIN'" class="flex items-center gap-2">
+                <Checkbox
+                    id="remember"
+                    v-model:checked="(form as any).remember"
+                    :tabindex="1"
+                />
+                <Label for="remember" class="cursor-pointer text-sm font-normal">
+                    Se souvenir de moi
+                </Label>
+            </div>
         </div>
 
         <Button type="submit" class="mt-2 w-full" tabindex="5" :disabled="form.processing">
